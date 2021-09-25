ad3b426a435d748d81a10298632e54c60a591717
Chayan Dashora <chayan@mahitechllc.com>
2021-09-25 21:34:55 +0530

    Intial UI Commit

---
+++ flutter_application/lib/main.dart
@@ -0,0 +1,27 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_application/home_page.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text("My Application"),
+        ),
+        body: Center(
+          child: Container(
+            child: Text("Hello"),
+          ),
+        ),
+        drawer: Drawer(),
+      ),
+    );
+  }
+}
